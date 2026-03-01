b82bd07cef562102d26b9ed8c3e20ca365a823ef
PolinaGr <polinasoukh@gmail.com>
2026-03-01 21:43:58 +0200

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,65 @@
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+import 'dotenv/config';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+
+const PORT = process.env.PORT ?? 3000;
+
+// Логування часу
+app.use((req, res, next) => {
+  console.log(`Time: ${new Date().toLocaleString()}`);
+  next();
+});
+
+// Перший маршрут
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello world!' });
+});
+
+// Маршрут для тестування middleware помилки
+app.get('/test-error', (req, res) => {
+  // Штучна помилка для прикладу
+  throw new Error('Something went wrong');
+});
+
+// Middleware 404 (після всіх маршрутів)
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+// Middleware для обробки помилок (останнє)
+app.use((err, req, res, next) => {
+  console.error(err);
+  const isProd = process.env.NODE_ENV === 'production';
+
+  res.status(500).json({
+    message: isProd
+      ? 'Something went wrong. Please try again later.'
+      : err.message,
+  });
+});
+
+// Запуск сервера
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
