bb7b9184d433c41b0606751ec0dcf43c5693d10e
PolinaGr <polinasoukh@gmail.com>
2026-03-18 19:15:42 +0200

    update mongoose

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -1,6 +1,6 @@
 import { Joi, Segments } from 'celebrate';
 import { isValidObjectId } from 'mongoose';
-import { TAGS } from '../constants/tags';
+import { TAGS } from '../constants/tags.js';
 
 // GET
 export const getAllNotesSchema = {
