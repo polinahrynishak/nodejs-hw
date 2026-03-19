77821a864839c294cf302a23e6dab1c6453bf27e
PolinaGr <polinasoukh@gmail.com>
2026-03-19 14:59:42 +0200

    add user logout

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -49,3 +49,18 @@ export const loginUser = async (req, res) => {
 
   res.status(200).json(user);
 };
+
+// Логаут
+export const logoutUser = async (req, res) => {
+  const { sessionId } = req.cookies;
+
+  if (sessionId) {
+    await Session.deleteOne({ _id: sessionId });
+  }
+
+  res.clearCookie('sessionId');
+  res.clearCookie('accessToken');
+  res.clearCookie('refreshToken');
+
+  res.status(204).send();
+};
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,10 @@
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
-import { loginUser, registerUser } from '../controllers/authController.js';
+import {
+  loginUser,
+  registerUser,
+  logoutUser,
+} from '../controllers/authController.js';
 import {
   loginUserSchema,
   registerUserSchema,
@@ -10,5 +14,6 @@ const router = Router();
 
 router.post('/auth/register', celebrate(registerUserSchema), registerUser);
 router.post('/auth/login', celebrate(loginUserSchema), loginUser);
+router.post('/auth/logout', logoutUser);
 
 export default router;
