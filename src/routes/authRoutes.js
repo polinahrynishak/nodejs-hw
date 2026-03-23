d568fd06fefb52371ec3f92d414840a5fedd987b
PolinaGr <polinasoukh@gmail.com>
2026-03-23 17:30:06 +0200

    add reset password

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,10 +5,12 @@ import {
   logoutUser,
   registerUser,
   refreshUserSession,
+  requestResetEmail,
 } from '../controllers/authController.js';
 import {
   loginUserSchema,
   registerUserSchema,
+  requestResetEmailSchema,
 } from '../validations/authValidation.js';
 
 const router = Router();
@@ -17,5 +19,10 @@ router.post('/auth/register', celebrate(registerUserSchema), registerUser);
 router.post('/auth/login', celebrate(loginUserSchema), loginUser);
 router.post('/auth/logout', logoutUser);
 router.post('/auth/refresh', refreshUserSession);
+router.post(
+  '/auth/request-reset-email',
+  celebrate(requestResetEmailSchema),
+  requestResetEmail,
+);
 
 export default router;
